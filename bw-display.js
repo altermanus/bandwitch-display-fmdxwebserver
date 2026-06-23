e7acbf1bbcad83b93459d737a117fcc69428ed56
altermanus <63774806+altermanus@users.noreply.github.com>
2026-06-23 08:41:33 +0200

    Create bw-display.js

---
+++ bw-display.js
@@ -0,0 +1,14 @@
+// Plugin configuration, this is used in the administration when plugins are loaded
+var pluginConfig = {
+    name: 'Bandwitch display',
+    version: '0.1b',
+    author: 'altermanus',
+    frontEndPath: 'bw-display/bw-display.js'
+}
+
+// Backend (server) changes can go here...
+
+// Don't change anything below here if you are making your own plugin
+module.exports = {
+    pluginConfig
+} 
